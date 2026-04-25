be849b2c958dfa88034e5fcceb2a01eef69f11d1
Luke Washington Garces <lukiesluke@gmail.com>
2026-04-25 14:31:53 +0800

    - Applied LazyListState() on list when screen rotate user will stay on the position of the list

--- app/src/main/java/com/lukegarces/openweather/view/WeatherListScreen.kt
+++ app/src/main/java/com/lukegarces/openweather/view/WeatherListScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -34,9 +36,13 @@ import com.lukegarces.openweather.viewmodel.WeatherViewModel
 
 @Composable
 fun WeatherListScreen(viewModel: WeatherViewModel) {
-
     val state by viewModel.weatherListState.collectAsState()
 
+    val listState = rememberSaveable(
+        saver = LazyListState.Saver
+    ) {
+        LazyListState()
+    }
     LaunchedEffect(Unit) {
         viewModel.loadWeatherList("manila")
     }
@@ -53,8 +59,7 @@ fun WeatherListScreen(viewModel: WeatherViewModel) {
             }
 
             is ApiResult.Success -> {
-
-                LazyColumn {
+                LazyColumn(state = listState) {
                     items(result.data.list) { item ->
                         Card(
                             modifier = Modifier
