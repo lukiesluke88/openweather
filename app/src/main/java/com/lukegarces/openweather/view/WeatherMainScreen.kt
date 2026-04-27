93346493b9a893379e327ef00778c5b13c08e790
Luke Washington Garces <lukiesluke@gmail.com>
2026-04-27 13:41:00 +0800

    -Separate the Header content from the navigation drawer

--- app/src/main/java/com/lukegarces/openweather/view/WeatherMainScreen.kt
+++ app/src/main/java/com/lukegarces/openweather/view/WeatherMainScreen.kt
@@ -1,15 +1,6 @@
 package com.lukegarces.openweather.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Divider
@@ -17,7 +8,6 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -32,14 +22,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lukegarces.openweather.R
 import com.lukegarces.openweather.data.model.LocationHelper
@@ -47,6 +31,7 @@ import com.lukegarces.openweather.data.model.User
 import com.lukegarces.openweather.data.remote.RetrofitInstance
 import com.lukegarces.openweather.data.repository.WeatherRepository
 import com.lukegarces.openweather.view.component.ConfirmAlertDialog
+import com.lukegarces.openweather.view.component.HeaderNavigationContent
 import com.lukegarces.openweather.view.component.WeatherTabsContent
 import com.lukegarces.openweather.viewmodel.WeatherViewModel
 import com.lukegarces.openweather.viewmodel.WeatherViewModelFactory
@@ -97,54 +82,7 @@ fun WeatherMainScreen(user: User, onLogout: () -> Unit) {
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            start = 16.dp,
-                            top = 50.dp,
-                            end = 16.dp
-                        ),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .size(80.dp)
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colorScheme.primaryContainer),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Text(
-                            text = user.name.firstOrNull()?.uppercase() ?: "?",
-                            style = MaterialTheme.typography.headlineMedium,
-                            fontWeight = FontWeight.Bold,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                    }
-
-                }
-                Column(
-                    modifier = Modifier.padding(
-                        start = 16.dp,
-                        top = 8.dp,
-                        end = 16.dp,
-                        bottom = 16.dp
-                    )
-                ) {
-
-
-                    Spacer(modifier = Modifier.height(12.dp))
-
-                    Text(
-                        text = user.name,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-
-                    Text(
-                        text = user.email,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
+                HeaderNavigationContent(user)
 
                 Divider()
 
